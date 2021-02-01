f400fff9af06c5396ccb06353b9d4caf8172bb4c
aiswalkar3 <aiswalkar3@gmail.com>
2021-01-31 21:48:53 -0500

    commiting fixes to WebSecurityConfiguration

--- projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/security/UserDetailsServiceImpl.java
+++ projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/security/UserDetailsServiceImpl.java
@@ -1,8 +1,14 @@
 package com.example.demo.security;
 
+import java.util.ArrayList;
 import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -12,18 +18,23 @@ import com.example.demo.model.persistence.User;
 import com.example.demo.model.persistence.repositories.UserRepository;
 
 @Service
-public class UserDetailsServiceImpl implements UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService
+{
 
     @Autowired
     private UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException, DataAccessException
+    {
         User user = userRepository.findByUsername(username);
+
         if (user == null) {
             throw new UsernameNotFoundException(username);
         }
+
         return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
                 Collections.emptyList());
     }
+
 }
